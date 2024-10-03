2c827c772dafbad774cc941173e492d3dae900ef
Emil Ilalov <e.ilalov2810@gmail.com>
2024-10-03 21:12:51 +0300

    Finalization of all existing modules in the 'tests' directory. Adding import 'from .test_main import list_dict' to test_processing, for some reason pytest was giving error 'fixture 'list_dict' not found' didn't want to pass without importing

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
 from typing import List, Dict
 
 
-def filter_by_state(info: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(info: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Функция возвращает список состояний банковских операций."""
     new_list_data = []
     for item in info:
-        if item.get('state') == state:
+        if item.get("state") == state:
             new_list_data.append(item)
 
     return new_list_data
 
 
-def sort_by_date(info: List[Dict], sort_data: bool = True) -> List[Dict]:
+def sort_by_date(list_dict: List[Dict], sort_data: bool = True) -> List[Dict]:
     """Функция возвращает отсортированный список по дате"""
-    return sorted(info, key=lambda item: item['date'], reverse=sort_data)
+    return sorted(list_dict, key=lambda item: item["date"], reverse=sort_data)
